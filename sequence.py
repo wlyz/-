1c34cf46f04736a7fa04b0088566820bca720f92
Chongfeng <chongfeng.zero@gmail.com>
2020-04-30 11:14:31 +0800

    create static function

--- sequence.py
+++ sequence.py
@@ -1,27 +1,39 @@
-class Sequence():
+def isSeq(seq):  # 判断是否ATCG序列
+    dictSeq = {}
+    for s in seq:
+        dictSeq[s] = seq.count(s)
+    if len(dictSeq) != 4:
+        return False
+    else:
+        return True
+
+
+def isLegal(seq):  # 判断有无非法酶切位点
     legal = True
-    normalization = True
-    illegalWords = {'GAATTC': None, 'TCTAGA': None, 'ACTAGT': None, 'CTGCAG': None, 'GCGGCCGC': None}
+    illegal_words = {'GAATTC': None, 'TCTAGA': None, 'ACTAGT': None, 'CTGCAG': None, 'GCGGCCGC': None}
+    for sites in ['GAATTC', 'TCTAGA', 'ACTAGT', 'CTGCAG', 'GCGGCCGC']:
+        position = seq.find(sites)
+        if position != -1:
+            legal = False
+            illegal_words[sites] = position
+    return legal, illegal_words
+
+
+def delete():  # 酶切位点删除
+    pass
+
+
+def add_prefix_suffix():  # 序列前后缀标准化
+    pass
+
+
+class Sequence:
+    # seq = ''
+    # legal = True
+    # normalization = True
+    # illegalWords = {'GAATTC': None, 'TCTAGA': None, 'ACTAGT': None, 'CTGCAG': None, 'GCGGCCGC': None}
 
     def __init__(self, seq):
-
-        self.isLegal(seq)
-
-        # 判断标准化
-        if self.legal:
-            print('legal seq')
-
-    def isLegal(self, seq):  # 判断有无非法酶切位点
-        for sites in ['GAATTC', 'TCTAGA', 'ACTAGT', 'CTGCAG', 'GCGGCCGC']:
-            position = seq.find(sites)
-            if position != -1:
-                self.legal = False
-                self.illegalWords[sites] = position
-        pass
-
-    def delete(self, seq):  # 酶切位点删除
-        pass
-
-    def add_prefix_suffix(self):  # 序列前后缀标准化
-        pass
+        self.seq = seq.upper()
+        self.legal, self.illegalWords = isLegal(seq)
 
